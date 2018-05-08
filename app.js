7c7d0d026be0f7532961ef45bf04b2972cad5fff
ejmeri <elmerisilva@hotmail.com>
2018-05-08 20:04:38 -0300

    get err seque

--- app.js
+++ app.js
@@ -20,5 +20,7 @@ db.sequelize.sync().then(function () {
 	server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
 		var addr = server.address();
 		console.log("Connected to ", addr.address + ":" + addr.port);
+	}).catch(err, () => {
+		console.log(err);
 	});
 });
