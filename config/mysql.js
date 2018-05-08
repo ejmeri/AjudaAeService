b04fd4b133e1b5aba912514d3891087dbd776808
ejmeri <elmerisilva@hotmail.com>
2018-05-08 20:17:03 -0300

    change database

--- config/mysql.js
+++ config/mysql.js
@@ -1,11 +1,11 @@
 module.exports = {
-    database: "ajudaae_db",
-    username: "root",
-    password: "Googleamazon!123", //Google@amazon!123
+    database: "d8e77t9rho4bnd", // "ajudaae_db",
+    username: "lrgmoarmphbxgt",
+    password: "afb6436b63658a6ba42878084c538bffafe07a262beacaeb9785681e98d8eb43", //Google@amazon!123
     params: {
-        host: 'ajudaaedb.cabz5p5rbadb.us-east-2.rds.amazonaws.com',
-        port: '3306',
-        dialect: "mysql",
+        host: 'ec2-174-129-41-64.compute-1.amazonaws.com', //'ajudaaedb.cabz5p5rbadb.us-east-2.rds.amazonaws.com',
+        port: '5432',
+        dialect: "postgres",
         define: {
             underscored: true,
             freezeTableName: true,
