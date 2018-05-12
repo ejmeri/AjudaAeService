91beb3d76d39b55d4b8ef66f4184f6b689e2de93
ejmeri <elmerisilva@hotmail.com>
2018-05-12 14:49:44 -0300

    try connecting mysql

--- config/mysql.js
+++ config/mysql.js
@@ -1,7 +1,7 @@
 module.exports = {
-    database:  'ajudaae_db',//"d8e77t9rho4bnd", // "ajudaae_db",
-    username:  'root', //"lrgmoarmphbxgt",
-    password: 'Googleamazon!123',  //"afb6436b63658a6ba42878084c538bffafe07a262beacaeb9785681e98d8eb43", //Google@amazon!123
+    database: 'ajudaae_db', //"d8e77t9rho4bnd", // "ajudaae_db",
+    username: 'root', //"lrgmoarmphbxgt",
+    password: 'Googleamazon!123', //"afb6436b63658a6ba42878084c538bffafe07a262beacaeb9785681e98d8eb43", //Google@amazon!123
     params: {
         host: 'ajudaaedb.cabz5p5rbadb.us-east-2.rds.amazonaws.com', //'ec2-174-129-41-64.compute-1.amazonaws.com', //
         port: '3306', //'5432',
@@ -9,6 +9,11 @@ module.exports = {
         define: {
             underscored: true,
             freezeTableName: true,
-        }
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 1 // Keep this very low or it'll make all Lambda requests take longer
+        },
     }
 }
