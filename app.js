ff1f6c2c3e6a7a82feb693bf53d421e34fb06fe7
ejmeri <elmerisilva@hotmail.com>
2018-05-17 15:41:18 -0300

    route not found

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
+var path = require('path');
+var engine = require('consolidate');
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 var db = require('./models'); // database and tables
 
@@ -17,11 +25,10 @@ consign()
     .into(app);
 
 
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-// //    res.status(404).send({error: 'Router not found', status: false});
-// 	res.sendFile('error404/index.html');
-// });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	res.render('notfound/index');
+});
 	
 db.sequelize.sync().then(function () {
 	server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
