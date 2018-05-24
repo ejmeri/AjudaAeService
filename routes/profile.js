327c4c0ac2af0cf3f84cf9e0319e1ef877b97156
ejmeri <elmerisilva@hotmail.com>
2018-05-24 20:16:00 -0300

    change profile.photo to base64

--- routes/profile.js
+++ routes/profile.js
@@ -35,7 +35,7 @@ router.get('/:user_id', async (req, res) => {
     }
 
     // console.log(profile.photo.toString('Base64'));
-    // profile.photo =  profile.photo.toString('Base64');
+    profile.photo =  profile.photo.toString('Base64');
     
     res.send({profile: profile ,status: true});
 
